8c43ded6c4503aaaf0d92ef05751fd26a7eeb608
E Siltu <machinevaneray@192.168.68.102>
2024-04-23 19:27:03 +0200

    Tue Apr 23 19:27:03 CEST 2024

--- components/FormSignIn.tsx
+++ components/FormSignIn.tsx
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { Formik } from 'formik';
+import ValidationSchema from 'lib/ValidationSchema';
 
 export default function FormSignIn() {
   return (
     <View style={styles.formHeaderContainer}>
       <Text style={styles.formHeaderTxt}>Sign in</Text>
+      <KeyboardAvoidingView>
+        <Formik
+          onSubmit={() => console.log('something')}
+          initialValues={{ email: '', password: '' }}
+          validationSchema={ValidationSchema}></Formik>
+      </KeyboardAvoidingView>
     </View>
   );
 }
