b05d6d3a76c2bb71263b45e7351108dde91d2c6d
E Siltu <machinevaneray@Laptop-van-Siltu.local>
2024-04-23 15:32:42 +0200

    Tue Apr 23 15:32:42 CEST 2024

--- routers/PageRouter.ts
+++ routers/PageRouter.ts
@@ -1,3 +1,5 @@
 export { default as OnboardingPages } from '../screens/pages/OnboardingPages';
 export { default as Details } from '../screens/details';
 export { default as Overview } from '../screens/overview';
+export { default as SignIn } from '../screens/auth/SignIn';
+export { default as SignUp } from '../screens/auth/SignUp';
