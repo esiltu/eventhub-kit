5cbda22a18e53b37d6ac414c3bfb8ac7228114cb
E Siltu <machinevaneray@192.168.68.102>
2024-04-23 19:07:47 +0200

    Tue Apr 23 19:07:47 CEST 2024

---
+++ components/SafeView.tsx
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet } from 'react-native';
+import React, { ReactNode } from 'react';
+
+type SafeViewProps = {
+  children: ReactNode;
+};
+
+export default function SafeView({ children }: SafeViewProps) {
+  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- screens/auth/SignIn.tsx
+++ screens/auth/SignIn.tsx
@@ -1,19 +1,13 @@
-import { StyleSheet, SafeAreaView } from 'react-native';
 import React from 'react';
 import Header from 'components/Header';
+import FormSignIn from 'components/FormSignIn';
+import SafeView from '../../components/SafeView';
 
-const SignIn = () => {
+export default function SignIn() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeView>
       <Header />
-    </SafeAreaView>
+      <FormSignIn />
+    </SafeView>
   );
-};
-
-export default SignIn;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+}
