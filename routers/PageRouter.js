52fdf8d91ec5ae446c9607b9bc0fd1d9acbcd504
E Siltu <machinevaneray@192.168.68.102>
2024-05-06 20:52:54 +0200

    Mon May  6 20:52:54 CEST 2024

--- routers/PageRouter.js
+++ routers/PageRouter.js
@@ -1,6 +1,4 @@
 export { default as OnboardingPages } from '../screens/pages/OnboardingPages';
-export { default as Details } from '../screens/details';
-export { default as Overview } from '../screens/overview';
 export { default as SignIn } from '../screens/auth/SignIn';
 export { default as SignUp } from '../screens/auth/SignUp';
 export { default as Home } from '../screens/tab/Home';
