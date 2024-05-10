7b5fe578087254e925fc6f9e3b9c0710f5bfb819
ES <erays@192.168.68.100>
2024-05-10 22:02:04 +0200

    Fri May 10 22:02:04 CEST 2024

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-import React, { useEffect } from 'react';
-import { Platform } from 'react-native';
+import React from 'react';
 import axios from 'axios';
 import { AuthProvider } from 'context/AuthContextProvider';
 import 'react-native-gesture-handler';
-import * as QuickActions from 'expo-quick-actions';
 import RootStack from './navigation';
 import 'react-native-gesture-handler';
 import { decode as atob } from 'base-64';
@@ -11,20 +9,8 @@ global.atob = atob;
 
 axios.defaults.baseURL = 'https://esdevelops.pro';
 
-// Test commit 
 
 export default function App() {
-  useEffect(() => {
-    QuickActions.setItems([
-      {
-        title: 'Wacht! Niet wissen!',
-        subtitle: 'We zijn hier om te helpen',
-        icon: Platform.OS === 'ios' ? 'symbol:person.crop.circle.badge.questionmark' : undefined,
-        id: '0',
-        params: { href: '/help' },
-      },
-    ]);
-  }, []);
   return (
     <AuthProvider>
       <RootStack />
